cc4204dcb54c700e6b792b688d415feb1d966948
[Juan] <[jhonybiology@ciencias.unam.mx]>
2025-08-17 19:12:31 -0600

    operaciones aritmeticas

--- hellogit_juan.py
+++ hellogit_juan.py
@@ -1,6 +1,16 @@
-print("Hello world")
-
-
+# Printing Hello world
+print("Hello, World!")
+# Simple Arithmetic Operations
+a = 37323
+b = 39373
+# Adittion
+print(a+b)
+# Substraction
+print(b-a)
+# Multiplication
+print(a*b)
+# Division
+print(a/b)
 
 
 
