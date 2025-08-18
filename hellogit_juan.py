73ec11fe1c6dbc5401a1c16de879c35d2401b2d5
[Juan] <[jhonybiology@ciencias.unam.mx]>
2025-08-18 12:51:13 -0600

    pandas excersies

--- hellogit_juan.py
+++ hellogit_juan.py
@@ -1,3 +1,4 @@
+## Week 1
 # Printing Hello world
 print("Hello, World!")
 # Simple Arithmetic Operations
@@ -11,12 +12,20 @@ print(b-a)
 print(a*b)
 # Division
 print(a/b)
-# Absolut valu of a number
+# Absolute value of a number
 def valor_absoluto(x):
     return x if x >= 0 else -x 
 
 print(valor_absoluto(-13))
 
+## Week 2
+# Import pandas
+import pandas as  pd
+# Import dataset 
+df = pd.read_csv("Terminos_lagoon_TA_DIC_2023_RawData.csv")
+print(df.head(5))
+print(df.tail(5))
+print(df.info())
 
 
 
