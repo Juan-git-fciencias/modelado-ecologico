64cb5209afd38f96ea2a98659ffcfa64ee0ec7ab
[Juan] <[jhonybiology@ciencias.unam.mx]>
2025-08-20 14:39:24 -0600

    Creación de excel

--- hellogit_juan.py
+++ hellogit_juan.py
@@ -19,13 +19,38 @@ def valor_absoluto(x):
 print(valor_absoluto(-13))
 
 ## Week 2
-# Import pandas
+# Import packages
+import numpy as np
 import pandas as  pd
 # Import dataset 
 df = pd.read_csv("Terminos_lagoon_TA_DIC_2023_RawData.csv")
 print(df.head(5))
 print(df.tail(5))
 print(df.info())
-
+# New column "TA_DIC_ratio"
+df["TA_DIC_ratio"] = df["ta_micromol_kg"]/df["dic_micromol_kg"]
+print(df.head(6))
+# Mean of "TA_DIC_ratio" per season 
+print(df["season"].head())
+season_mean = df.groupby("season")["TA_DIC_ratio"].mean()
+print(season_mean)
+# Mean of "TA_DIC_ratio"per area
+area_mean = df.groupby("area")["TA_DIC_ratio"].mean()
+print(area_mean)
+# Std of "TA_DIC_ratio" per season and area
+season_std = df.groupby("season")["TA_DIC_ratio"].std()
+print(season_std)
+area_std = df.groupby("area")["TA_DIC_ratio"].std()
+print(area_std)
+# Create dataframe with objects
+TA_DIC_Season_Areas = pd.DataFrame({
+    "season_mean": season_mean,
+    "area_mean": area_mean, 
+    "season_std": season_std,
+    "area_std": area_std,
+})
+print(np.transpose(TA_DIC_Season_Areas))
+# Crearte excel with results 
+TA_DIC_Season_Areas.to_excel("TA_DIC_Season_Area.xlsx", sheet_name="Hoja 1", index=True)
 
 
